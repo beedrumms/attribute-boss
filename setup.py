c3e6e8d102aec01273e25481d2912fd3ea0f1d08
Brenna Drummond <Brenna.Drummond@ontario.ca>
2023-07-04 15:29:30 -0400

    small change to setups

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="AttributeBossPySpark",
-    version = "0.0.1",
+    version = "0.0.2",
     #version = version,
     include_package_data=True,
     description = "This package aims to allow for quick and basic standardization, extraction, and validation of key attributes",
@@ -31,8 +31,7 @@ setup(
     long_description_content_type="text/markdown",
     author = "BDRUMMOND",
     author_email = "b3drumms@gmail.com",
-    package_dir={"": "src"},
-    packages = find_packages(where="src", exclude=['tests']),
+    packages = find_packages(),
     #install_requires = requirements,
     install_requires = ['pandas', 'numpy', 'regex', 'pyspark'],
     python_requires = '>=3.8',
