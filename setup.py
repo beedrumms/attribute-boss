2d7ba82b5ffe9225e458fc993dff3f99a6075247
Brenna Drummond <Brenna.Drummond@ontario.ca>
2023-07-04 16:17:12 -0400

    version 0.0.3

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="AttributeBossPySpark",
-    version = "0.0.0",
+    version = "0.0.3",
     include_package_data=True,
     description = "This package aims to allow for quick and basic standardization, extraction, and validation of key attributes",
     long_description=long_description,
