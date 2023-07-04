b34736efc339a700ee9071c5df5cf8b05bca253a
yang <x2018uka@gmail.com>
2023-07-04 15:20:50 -0400

    add

---
+++ setup.py
@@ -0,0 +1,40 @@
+#%%
+from setuptools import setup, find_packages
+import pathlib
+###
+
+#%% get windows path for project 
+here = pathlib.Path(__file__).parent.resolve()
+###
+
+#%%
+#with open('version.txt', 'r') as f:
+#    version = f.read().strip()
+
+#%% NOT WORKING -- 
+#with open("requirements.txt") as f:
+#    requirements = f.read().splitlines()
+
+#%%
+long_description = (here / "README.md").read_text(encoding="utf-8")
+###
+
+#%%
+
+setup(
+    name="AttributeBossPySpark",
+    version = "0.0.1",
+    #version = version,
+    include_package_data=True,
+    description = "This package aims to allow for quick and basic standardization, extraction, and validation of key attributes",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author = "BDRUMMOND",
+    author_email = "b3drumms@gmail.com",
+    packages = find_packages(),
+    #install_requires = requirements,
+    install_requires = ['pandas', 'numpy', 'regex', 'pyspark'],
+    python_requires = '>=3.8',
+     extras_require={"tests": ["pytest", "pytest_spark"]})
+
+# %%
