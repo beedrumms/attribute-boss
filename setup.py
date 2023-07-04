75d644cd84d1247069b5ff7761c97344d6b8572a
Brenna Drummond <Brenna.Drummond@ontario.ca>
2023-07-04 17:16:44 -0400

    issue with building

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="AttributeBossPySpark",
-    version = "0.0.3",
+    version = "0.0.4",
     include_package_data=True,
     description = "This package aims to allow for quick and basic standardization, extraction, and validation of key attributes",
     long_description=long_description,
