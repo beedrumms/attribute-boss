dea1e3bf4287b8412cd148eb75b198c1c2d85164
Brenna Drummond <Brenna.Drummond@ontario.ca>
2023-12-03 18:28:11 -0500

    updating test calls to src to subfolder

--- tests/test_PyTools.py
+++ tests/test_PyTools.py
@@ -10,7 +10,7 @@ import os
 import sys
 
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../src")
-from PyTools import PyBoss # as boss
+from AttributeBoss.PyTools import PyBoss # as boss
 boss = PyBoss()
 
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
--- tests/test_SparkTools.py
+++ tests/test_SparkTools.py
@@ -11,7 +11,7 @@ import os
 import sys
 
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../src")
-from SparkTools import SparkBoss
+from AttributeBoss.SparkTools import SparkBoss
 boss = SparkBoss()
 
 
